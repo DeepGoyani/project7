7c61b3503d39f8c9f23da30570a322d7976d0ab2
DeepGoyani <177735104+DeepGoyani@users.noreply.github.com>
2026-02-17 17:49:43 +0530

    Add Index.tsx to source code

---
+++ src/pages/Index.tsx
@@ -0,0 +1,48 @@
+import { useState, useCallback } from "react";
+import Preloader from "@/components/Preloader";
+import CustomCursor from "@/components/CustomCursor";
+import ParticleField from "@/components/ParticleField";
+import Navbar from "@/components/Navbar";
+import Hero from "@/components/Hero";
+import MarqueeSection from "@/components/MarqueeSection";
+import HorizontalScroll from "@/components/HorizontalScroll";
+import ServicesList from "@/components/ServicesList";
+import TeamSection from "@/components/TeamSection";
+import BlogSection from "@/components/BlogSection";
+import FaqSection from "@/components/FaqSection";
+import AgencyFooter from "@/components/AgencyFooter";
+import SmoothScroll from "@/components/SmoothScroll";
+
+const Index = () => {
+  const [preloaderDone, setPreloaderDone] = useState(false);
+
+  const handlePreloaderComplete = useCallback(() => {
+    // Delay state change to let GSAP finish its animation before React touches the DOM
+    setTimeout(() => setPreloaderDone(true), 100);
+  }, []);
+
+  return (
+    <>
+      <CustomCursor />
+      <ParticleField />
+      {!preloaderDone && <Preloader onComplete={handlePreloaderComplete} />}
+      <SmoothScroll>
+        <Navbar />
+        <main className="relative z-10">
+          <section id="home">
+            <Hero />
+          </section>
+          <MarqueeSection />
+          <ServicesList />
+          <HorizontalScroll />
+          <TeamSection />
+          <BlogSection />
+          <FaqSection />
+          <AgencyFooter />
+        </main>
+      </SmoothScroll>
+    </>
+  );
+};
+
+export default Index;
